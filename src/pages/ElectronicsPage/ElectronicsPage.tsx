3b10c385cde0528652b2730f39fb9743c1c0b574
IvanDanilov03 <ivan_danilov@ukr.net>
2023-03-08 23:19:39 +0200

    feat: create empty ElectronicsPage

---
+++ src/pages/ElectronicsPage/ElectronicsPage.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { ElectronicsPageView } from "../ElectronicsPageView/ElectronicsPageView";
+
+const ElectronicsPage: FC = () => {
+  return <ElectronicsPageView />;
+};
+
+export default ElectronicsPage;
