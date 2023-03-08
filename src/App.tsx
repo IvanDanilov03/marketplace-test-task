a2cbfb1433b5438cca92866526cea8b001947a7a
IvanDanilov03 <ivan_danilov@ukr.net>
2023-03-08 23:03:04 +0200

    feat: add ThemeProvider to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {FC} from "react";
+import { CssBaseline, ThemeProvider, Typography } from "@mui/material";
+import { theme } from "./theme";
 
-function App() {
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+          <Typography>Text</Typography>
+      </ThemeProvider>
     </div>
   );
-}
+};
 
 export default App;
