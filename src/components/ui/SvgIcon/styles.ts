411372557f9c6ae6e54d1d5f6aeb8a7eda314df0
IvanDanilov03 <ivan_danilov@ukr.net>
2023-03-08 23:52:23 +0200

    feat: add SvgIcon component

---
+++ src/components/ui/SvgIcon/styles.ts
@@ -0,0 +1,16 @@
+import { SxStyles } from '../../../types/styles';
+
+export const styles: SxStyles = {
+  small: {
+    '&': {
+      width: '18px',
+      height: '18px',
+    },
+  },
+  medium: {
+    '&': {
+      width: '24px',
+      height: '24px',
+    },
+  },
+};
