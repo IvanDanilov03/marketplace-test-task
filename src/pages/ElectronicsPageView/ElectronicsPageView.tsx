311473b0fd6073675d47a2cb946104cc648aeefd
IvanDanilov03 <ivan_danilov@ukr.net>
2023-03-08 23:19:56 +0200

    feat: create empty ElectronicsPageView with styles

---
+++ src/pages/ElectronicsPageView/ElectronicsPageView.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+import { styles } from "./styles";
+
+interface ElectronicsPageViewProps {}
+
+export const ElectronicsPageView: FC<ElectronicsPageViewProps> = () => {
+  return <></>;
+};
