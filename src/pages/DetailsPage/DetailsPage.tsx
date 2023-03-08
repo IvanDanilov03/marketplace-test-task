ac3ef45cc5fefec5506360f327cbdab1b4622066
IvanDanilov03 <ivan_danilov@ukr.net>
2023-03-08 23:18:56 +0200

    feat: create empty DetailsPage

---
+++ src/pages/DetailsPage/DetailsPage.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { DetailsPageView } from "../DetailsPageView/DetailsPageView";
+
+const DetailsPage: FC = () => {
+  return <DetailsPageView />;
+};
+
+export default DetailsPage;
