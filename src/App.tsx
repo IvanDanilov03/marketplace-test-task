05804ea5eba0b7969fdc969450eb59ca63dd3352
IvanDanilov03 <ivan_danilov@ukr.net>
2023-03-08 23:21:55 +0200

    feat: add Routes to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import {FC} from "react";
-import { CssBaseline, ThemeProvider, Typography } from "@mui/material";
+import { FC } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
+import ElectronicsPage from "./pages/ElectronicsPage/ElectronicsPage";
+import DetailsPage from "./pages/DetailsPage/DetailsPage";
 
 const App: FC = () => {
   return (
     <div>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-          <Typography>Text</Typography>
+        <Routes>
+          <Route path="/" element={<Navigate replace to={`/electronics`} />} />
+          <Route path={`/electronics`} element={<ElectronicsPage />} />
+          <Route
+            path={`/electronics/:electronicsId`}
+            element={<DetailsPage />}
+          />
+        </Routes>
       </ThemeProvider>
     </div>
   );
