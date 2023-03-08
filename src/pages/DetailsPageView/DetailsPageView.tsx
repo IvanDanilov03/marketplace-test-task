d0a8c6facd7f3f3cf709288f972c4c7c3b2bc81f
IvanDanilov03 <ivan_danilov@ukr.net>
2023-03-08 23:19:29 +0200

    feat: create empty DetailsPageView with styles

---
+++ src/pages/DetailsPageView/DetailsPageView.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+import { styles } from "./styles";
+
+interface DetailsPageViewProps {}
+
+export const DetailsPageView: FC<DetailsPageViewProps> = () => {
+  return <></>;
+};
