0b74152c05fdf0334d9ba5301a3801b7a744b32f
IvanDanilov03 <ivan_danilov@ukr.net>
2023-03-08 23:30:56 +0200

    feat: create type Electronics

---
+++ src/types/api.d.ts
@@ -0,0 +1,13 @@
+export type Electronics = {
+  id: number;
+  title: string;
+  price: number;
+  brand: string;
+  previewDescription: string;
+  description: string;
+  imageUrl: string;
+  category: string;
+  type: string;
+  material: string;
+  design: string;
+};
