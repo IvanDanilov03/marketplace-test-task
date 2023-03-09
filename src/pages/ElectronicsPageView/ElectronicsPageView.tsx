03de4dccf9354d6fb1077450898f10d13afcd128
IvanDanilov03 <ivan_danilov@ukr.net>
2023-03-09 17:08:00 +0200

    feat: add BreadcrumbsSection and SortingSection to ElectronicsPageView

--- src/pages/ElectronicsPageView/ElectronicsPageView.tsx
+++ src/pages/ElectronicsPageView/ElectronicsPageView.tsx
@@ -1,15 +1,28 @@
 import { FC } from "react";
+import { BreadcrumbsSection } from "../../components/sections/BreadcrumbsSection";
+import { SortingSection } from "../../components/sections/SortingSection";
 import { Box } from "../../components/ui/Box";
 import { Header } from "../../components/unsorted/Header";
+import { useBreakpoints } from "../../hooks/useBreakpoints";
 
 import { styles } from "./styles";
 
 interface ElectronicsPageViewProps {}
 
 export const ElectronicsPageView: FC<ElectronicsPageViewProps> = () => {
+  const { isUpTablet } = useBreakpoints();
   return (
     <Box sx={styles.root}>
       <Header mobileTitle="Electronics" />
+      {isUpTablet && (
+        <BreadcrumbsSection
+          breadcrumbs={[
+            { title: "Home", link: "", id: "Home" },
+            { title: "Electronics", link: "", id: "Electronics" },
+          ]}
+        />
+      )}
+      <SortingSection />
     </Box>
   );
 };
--- src/pages/ElectronicsPageView/styles.ts
+++ src/pages/ElectronicsPageView/styles.ts
@@ -2,7 +2,7 @@ import { SxStyles } from "../../types/styles";
 import { basicTheme } from "../../theme";
 
 export const styles: SxStyles = {
-    root: {
-        bgcolor: 'greyPalette.100'
-    }
+  root: {
+    bgcolor: "greyPalette.100",
+  },
 };
