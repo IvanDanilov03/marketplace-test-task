6b3c2b3cd255e8ab813023fb5a6c763f4c046ff6
IvanDanilov03 <ivan_danilov@ukr.net>
2023-03-09 14:18:37 +0200

    refactor: update Header in ElectronicsPageView

--- src/pages/ElectronicsPageView/ElectronicsPageView.tsx
+++ src/pages/ElectronicsPageView/ElectronicsPageView.tsx
@@ -1,13 +1,15 @@
 import { FC } from "react";
 import { Box } from "../../components/ui/Box";
 import { Header } from "../../components/unsorted/Header";
-import { useBreakpoints } from "../../hooks/useBreakpoints";
 
 import { styles } from "./styles";
 
 interface ElectronicsPageViewProps {}
 
 export const ElectronicsPageView: FC<ElectronicsPageViewProps> = () => {
-  const { isUpTablet } = useBreakpoints();
-  return <Box sx={styles.root}>{isUpTablet && <Header />}</Box>;
+  return (
+    <Box sx={styles.root}>
+      <Header mobileTitle="Electronics" />
+    </Box>
+  );
 };
