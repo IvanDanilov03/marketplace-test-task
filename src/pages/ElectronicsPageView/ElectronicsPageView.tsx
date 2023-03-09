22508219d1092fab2dd55084c6039bd38a024b71
IvanDanilov03 <ivan_danilov@ukr.net>
2023-03-09 13:45:11 +0200

    feat: add Header to ElectronicsPageView

--- src/pages/ElectronicsPageView/ElectronicsPageView.tsx
+++ src/pages/ElectronicsPageView/ElectronicsPageView.tsx
@@ -1,9 +1,13 @@
 import { FC } from "react";
+import { Box } from "../../components/ui/Box";
+import { Header } from "../../components/unsorted/Header";
+import { useBreakpoints } from "../../hooks/useBreakpoints";
 
 import { styles } from "./styles";
 
 interface ElectronicsPageViewProps {}
 
 export const ElectronicsPageView: FC<ElectronicsPageViewProps> = () => {
-  return <></>;
+  const { isUpTablet } = useBreakpoints();
+  return <Box sx={styles.root}>{isUpTablet && <Header />}</Box>;
 };
