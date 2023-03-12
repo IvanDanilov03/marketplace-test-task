7fad3a71d93b04996c87f4f27b578c346ff93325
IvanDanilov03 <ivan_danilov@ukr.net>
2023-03-12 03:00:38 +0200

    feat: update DetailsPage

--- src/pages/DetailsPage/DetailsPage.tsx
+++ src/pages/DetailsPage/DetailsPage.tsx
@@ -1,8 +1,44 @@
 import { FC } from "react";
 import { DetailsPageView } from "../DetailsPageView/DetailsPageView";
+import { useParams } from "react-router-dom";
+import { DUMMY_DATA } from "../../utils/constants";
 
 const DetailsPage: FC = () => {
-  return <DetailsPageView />;
+  const { electronicsId } = useParams();
+
+  const electronicData = DUMMY_DATA.filter((item) => {
+    return item.id.toString() === electronicsId;
+  });
+
+  const {
+    id,
+    title,
+    price,
+    brand,
+    previewDescription,
+    description,
+    imageUrl,
+    category,
+    type,
+    material,
+    design,
+  } = electronicData[0];
+
+  return (
+    <DetailsPageView
+      id={id}
+      title={title}
+      price={price}
+      brand={brand}
+      previewDescription={previewDescription}
+      description={description}
+      imageUrl={imageUrl}
+      category={category}
+      type={type}
+      material={material}
+      design={design}
+    />
+  );
 };
 
 export default DetailsPage;
