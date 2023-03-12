671caf54fe3e460e001bda3a827c82f7289ee481
IvanDanilov03 <ivan_danilov@ukr.net>
2023-03-12 03:01:00 +0200

    feat: create empty RangeFilter

---
+++ src/components/unsorted/RangeFilter/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+import { styles } from "./styles";
+
+import { Box } from "../../ui/Box";
+
+export interface RangeFilterProps {}
+
+export const RangeFilter: FC<RangeFilterProps> = ({}) => {
+  return <Box></Box>;
+};
