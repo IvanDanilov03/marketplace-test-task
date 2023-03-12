9f1ac3ce8adbeb4ea0b11e540b9551a590704656
IvanDanilov03 <ivan_danilov@ukr.net>
2023-03-12 12:11:39 +0200

    feat: update links

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -38,7 +38,7 @@ export const DUMMY_DATA = [
       "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit ",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
-    imageUrl: "https://i.imgur.com/Zun44D7.jpg",
+    imageUrl: "https://i.imgur.com/0rQGruX.jpg",
     category: "Electronics",
     type: "Smart watch",
     material: "Aluminium",
@@ -53,7 +53,7 @@ export const DUMMY_DATA = [
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
-    imageUrl: "https://i.imgur.com/Zun44D7.jpg",
+    imageUrl: "https://i.imgur.com/UhCQCR6.png",
     category: "Electronics",
     type: "Smart watch",
     material: "Aluminium",
