eabb6ce4fcabbcec1c4b7538bd7af76843c83051
IvanDanilov03 <ivan_danilov@ukr.net>
2023-03-12 03:03:34 +0200

    feat: update ElectronicsPage

--- src/pages/ElectronicsPage/ElectronicsPage.tsx
+++ src/pages/ElectronicsPage/ElectronicsPage.tsx
@@ -1,8 +1,11 @@
 import { FC } from "react";
+
+import { DUMMY_DATA } from "../../utils/constants";
+
 import { ElectronicsPageView } from "../ElectronicsPageView/ElectronicsPageView";
 
 const ElectronicsPage: FC = () => {
-  return <ElectronicsPageView />;
+  return <ElectronicsPageView content={DUMMY_DATA} />;
 };
 
 export default ElectronicsPage;
