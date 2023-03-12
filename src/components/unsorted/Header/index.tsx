670ea9188b7623a9f5aebbfa712a0920d789bcaa
IvanDanilov03 <ivan_danilov@ukr.net>
2023-03-12 02:54:32 +0200

    feat: add onClick action to button in header

--- src/components/unsorted/Header/index.tsx
+++ src/components/unsorted/Header/index.tsx
@@ -1,13 +1,16 @@
-import { FC } from "react";
-import { ShoppingBagIcon } from "../../../components/icons/ShoppingBagIcon";
+import { FC, MouseEvent } from "react";
+import { useBreakpoints } from "../../../hooks/useBreakpoints";
+import { useNavigate } from "react-router-dom";
+
 import { Box } from "../../../components/ui/Box";
 import { Divider } from "../../../components/ui/Divider";
 import { Typography } from "../../../components/ui/Typography";
 import { Container } from "../../ui/Container";
-import { styles } from "./styles";
-import { useBreakpoints } from "../../../hooks/useBreakpoints";
 import { ArrowBackIcon } from "../../icons/ArrowBackIcon";
+import { ShoppingBagIcon } from "../../../components/icons/ShoppingBagIcon";
 import { IconButton } from "../../ui/IconButton";
+
+import { styles } from "./styles";
 
 interface DetailsPageViewProps {
   mobileTitle?: string;
@@ -15,12 +18,21 @@ interface DetailsPageViewProps {
 
 export const Header: FC<DetailsPageViewProps> = ({ mobileTitle }) => {
   const { isUpMobile, isUpTablet } = useBreakpoints();
+  const navigate = useNavigate();
+
+  const handleClick = (event: MouseEvent) => {
+    if (window.history.state === null) {
+      navigate(`/electronics`);
+    } else {
+      navigate(-1);
+    }
+  };
 
   return (
     <Box sx={styles.root} component="header">
       {isUpMobile && !isUpTablet && (
         <Container sx={styles.container}>
-          <IconButton sx={styles.backButton}>
+          <IconButton sx={styles.backButton} onClick={handleClick}>
             <ArrowBackIcon />
           </IconButton>
           {mobileTitle && <Typography variant="h5">{mobileTitle}</Typography>}
