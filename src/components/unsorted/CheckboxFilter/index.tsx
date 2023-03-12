decb96acea20eb0c04f67320d3797163bded8851
IvanDanilov03 <ivan_danilov@ukr.net>
2023-03-12 03:01:17 +0200

    feat: create empty CheckboxFilter

---
+++ src/components/unsorted/CheckboxFilter/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+import { styles } from "./styles";
+
+import { Box } from "../../ui/Box";
+
+export interface CheckboxFilterProps {}
+
+export const CheckboxFilter: FC<CheckboxFilterProps> = ({}) => {
+  return <Box></Box>;
+};
