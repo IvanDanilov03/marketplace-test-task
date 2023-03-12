9904c69d90617baf66a306612a7af604866858ae
IvanDanilov03 <ivan_danilov@ukr.net>
2023-03-12 03:00:25 +0200

    feat: update DetailsPageView

--- src/pages/DetailsPageView/DetailsPageView.tsx
+++ src/pages/DetailsPageView/DetailsPageView.tsx
@@ -1,9 +1,54 @@
 import { FC } from "react";
+import { useBreakpoints } from "../../hooks/useBreakpoints";
+
+import { Box } from "../../components/ui/Box";
+import { Header } from "../../components/unsorted/Header";
+import { Electronics } from "../../types/api";
+import { BreadcrumbsSection } from "../../components/sections/BreadcrumbsSection";
+import { DescriptionSection } from "../../components/sections/DescriptionSection";
 
 import { styles } from "./styles";
 
-interface DetailsPageViewProps {}
+export const DetailsPageView: FC<Electronics> = ({
+  id,
+  title,
+  price,
+  brand,
+  previewDescription,
+  description,
+  imageUrl,
+  category,
+  type,
+  material,
+  design,
+}) => {
+  const { isUpTablet } = useBreakpoints();
 
-export const DetailsPageView: FC<DetailsPageViewProps> = () => {
-  return <></>;
+  return (
+    <Box sx={styles.root}>
+      <Header mobileTitle="Electronics" />
+      {isUpTablet && (
+        <BreadcrumbsSection
+          breadcrumbs={[
+            { title: "Home", link: "", id: "Home" },
+            { title: "Electronics", link: "#/electronics", id: "Electronics" },
+            { title: `${type}`, link: "", id: `${id}` },
+          ]}
+        />
+      )}
+      <DescriptionSection
+        id={id}
+        title={title}
+        price={price}
+        brand={brand}
+        previewDescription={previewDescription}
+        description={description}
+        imageUrl={imageUrl}
+        category={category}
+        type={type}
+        material={material}
+        design={design}
+      />
+    </Box>
+  );
 };
