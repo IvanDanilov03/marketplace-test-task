7cb55220cd3c91fdd460e886419110c6bfb60176
IvanDanilov03 <ivan_danilov@ukr.net>
2023-03-13 12:38:06 +0200

    feat: update FiltersPanel

--- src/components/unsorted/FiltersPanel/index.tsx
+++ src/components/unsorted/FiltersPanel/index.tsx
@@ -3,14 +3,48 @@ import { FC } from "react";
 import { styles } from "./styles";
 
 import { Box } from "../../ui/Box";
-import { Typography } from "../../ui/Typography";
+import { Divider } from "../../ui/Divider";
+import { CheckboxFilter } from "../CheckboxFilter";
+import { RangeFilter } from "../RangeFilter";
+import { useBreakpoints } from "../../../hooks/useBreakpoints";
 
-export interface FiltersPanelProps {}
+export interface FiltersPanelProps {
+  filterTitle: string;
+  uniqueFilter: string[];
+  selectedFilter: string[];
+  setSelectedFilter: (value: React.SetStateAction<string[]>) => void;
+  selectedRange: number[];
+  setSelectedRange: (value: React.SetStateAction<number[]>) => void;
+}
 
-export const FiltersPanel: FC<FiltersPanelProps> = ({}) => {
+export const FiltersPanel: FC<FiltersPanelProps> = ({
+  filterTitle,
+  uniqueFilter,
+  selectedFilter,
+  setSelectedFilter,
+  selectedRange,
+  setSelectedRange,
+}) => {
+  const { isUpTablet } = useBreakpoints();
   return (
     <Box sx={styles.root}>
-      <Typography>Filters</Typography>
+      <Box sx={styles.checkboxFilter}>
+        <Divider sx={styles.topDivider} />
+        <CheckboxFilter
+          filterTitle={filterTitle}
+          uniqueFilter={uniqueFilter}
+          selectedFilter={selectedFilter}
+          setSelectedFilter={setSelectedFilter}
+        />
+      </Box>
+      <Box sx={styles.rangeFilter}>
+        {isUpTablet && <Divider sx={styles.bottomDivider} />}
+
+        <RangeFilter
+          selectedRange={selectedRange}
+          setSelectedRange={setSelectedRange}
+        />
+      </Box>
     </Box>
   );
 };
--- src/components/unsorted/FiltersPanel/styles.ts
+++ src/components/unsorted/FiltersPanel/styles.ts
@@ -1,7 +1,28 @@
 import { SxStyles } from "../../../types/styles";
+import { basicTheme } from "../../../theme";
 
 export const styles: SxStyles = {
   root: {
-    bgcolor: "greyPalette.500",
+    display: "flex",
+    flexDirection: "column",
+  },
+  topDivider: {
+    mb: 3,
+  },
+  rangeFilter: {
+    width: "50%",
+    order: -1,
+    mb: 5,
+    [basicTheme.breakpoints.up("tablet")]: {
+      order: 1,
+      mb: 0,
+      width: "100%",
+    },
+  },
+  bottomDivider: {
+    my: 3,
+    [basicTheme.breakpoints.up("tablet")]: {
+      mt: 15,
+    },
   },
 };
