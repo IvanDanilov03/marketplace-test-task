661398592851a4625ea1db3592440029b5a04f2a
IvanDanilov03 <ivan_danilov@ukr.net>
2023-03-13 12:39:11 +0200

    feat: update ElectronicsPageView

--- src/pages/ElectronicsPageView/ElectronicsPageView.tsx
+++ src/pages/ElectronicsPageView/ElectronicsPageView.tsx
@@ -1,5 +1,6 @@
-import { FC } from "react";
+import { FC, useState, useEffect, useCallback, useMemo } from "react";
 import { useBreakpoints } from "../../hooks/useBreakpoints";
+import { useSearchParams } from "react-router-dom";
 
 import { Box } from "../../components/ui/Box";
 import { Header } from "../../components/unsorted/Header";
@@ -13,12 +14,83 @@ import { styles } from "./styles";
 
 interface ElectronicsPageViewProps {
   content: Electronics[];
+  uniqueFilterNames: string[];
 }
 
 export const ElectronicsPageView: FC<ElectronicsPageViewProps> = ({
   content,
+  uniqueFilterNames,
 }) => {
   const { isUpTablet } = useBreakpoints();
+
+  // const [searchParams, setSearchParams] = useSearchParams();
+
+  const [filteredContent, setFilteredContent] = useState(content);
+  const [sortFromLowToHigh, setSortFromLowToHigh] = useState(true);
+  const [selectedBrand, setSelectedBrand] = useState<string[]>([]);
+  const [selectedRange, setSelectedRange] = useState([100, 7000]);
+
+  const filterData = useCallback(() => {
+    const findFilteredData = (
+      array: Electronics[],
+      filters: string[],
+      lowToHigh: boolean
+    ) => {
+      if (filters.length === 0 && lowToHigh) {
+        console.log(lowToHigh, "1");
+        const sortedRangeArray = array.filter((item) => {
+          return item.price < selectedRange[1] && item.price > selectedRange[0];
+        });
+        return sortedRangeArray.sort((a, b) => a.price - b.price);
+      }
+      if (filters.length === 0 && !lowToHigh) {
+        const sortedRangeArray = array.filter((item) => {
+          return item.price < selectedRange[1] && item.price > selectedRange[0];
+        });
+        return sortedRangeArray.sort((a, b) => b.price - a.price);
+      }
+      if (filters.length !== 0 && lowToHigh) {
+        const filteredByBrandData = filters
+          .map((item) =>
+            array.filter((electronic) => {
+              return electronic.brand === item;
+            })
+          )
+          .flat();
+        console.log(lowToHigh, "3");
+        const sortedRangeArray = filteredByBrandData.filter((item) => {
+          return item.price < selectedRange[1] && item.price > selectedRange[0];
+        });
+        return sortedRangeArray.sort((a, b) => a.price - b.price);
+      }
+      if (filters.length !== 0 && !lowToHigh) {
+        const filteredByBrandData = filters
+          .map((item) =>
+            array.filter((electronic) => {
+              return electronic.brand === item;
+            })
+          )
+          .flat();
+        console.log(lowToHigh, "4");
+        const sortedRangeArray = filteredByBrandData.filter((item) => {
+          return item.price < selectedRange[1] && item.price > selectedRange[0];
+        });
+        return sortedRangeArray.sort((a, b) => b.price - a.price);
+      }
+      return array;
+    };
+
+    setFilteredContent(
+      findFilteredData(content, selectedBrand, sortFromLowToHigh)
+    );
+  }, [content, sortFromLowToHigh, selectedBrand, selectedRange]);
+
+  useEffect(() => {
+    filterData();
+  }, [filterData]);
+
+  console.log(filteredContent);
+
   return (
     <Box sx={styles.root}>
       <Header mobileTitle="Electronics" />
@@ -30,8 +102,25 @@ export const ElectronicsPageView: FC<ElectronicsPageViewProps> = ({
           ]}
         />
       )}
-      <SortingSection />
-      <FilterElectronicsListSection content={content} />
+      <SortingSection
+        sortFromLowToHigh={sortFromLowToHigh}
+        setSortFromLowToHigh={setSortFromLowToHigh}
+        filterTitle="Brands"
+        uniqueFilter={uniqueFilterNames}
+        selectedFilter={selectedBrand}
+        setSelectedFilter={setSelectedBrand}
+        selectedRange={selectedRange}
+        setSelectedRange={setSelectedRange}
+      />
+      <FilterElectronicsListSection
+        content={filteredContent}
+        filterTitle="Brands"
+        uniqueFilter={uniqueFilterNames}
+        selectedFilter={selectedBrand}
+        setSelectedFilter={setSelectedBrand}
+        selectedRange={selectedRange}
+        setSelectedRange={setSelectedRange}
+      />
     </Box>
   );
 };
