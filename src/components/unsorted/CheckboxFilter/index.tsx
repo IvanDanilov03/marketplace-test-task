067de9f57af55c52a45eac86f44f4bf81335e564
IvanDanilov03 <ivan_danilov@ukr.net>
2023-03-13 12:37:22 +0200

    feat: update CheckboxFilter

--- src/components/unsorted/CheckboxFilter/index.tsx
+++ src/components/unsorted/CheckboxFilter/index.tsx
@@ -1,11 +1,128 @@
-import { FC } from "react";
-
-import { styles } from "./styles";
+import { FC, useState } from "react";
+import { useBreakpoints } from "../../../hooks/useBreakpoints";
 
 import { Box } from "../../ui/Box";
+import { Typography } from "../../ui/Typography";
+import { ArrowUpIcon } from "../../icons/ArrowUpIcon";
+import { FormControl } from "../../ui/FormControl";
+import { FormGroup } from "../../ui/FormGroup";
+import { Checkbox } from "../../ui/Checkbox";
+import { FormControlLabel } from "../../ui/FormControlLabel";
 
-export interface CheckboxFilterProps {}
+import { styles } from "./styles";
+import { IconButton } from "../../ui/IconButton";
+import { ArrowDownIcon } from "../../icons/ArrowDownIcon";
+import { Button } from "../../ui/Button";
 
-export const CheckboxFilter: FC<CheckboxFilterProps> = ({}) => {
-  return <Box></Box>;
+export interface CheckboxFilterProps {
+  filterTitle: string;
+  uniqueFilter: string[];
+  selectedFilter: string[];
+  setSelectedFilter: (value: React.SetStateAction<string[]>) => void;
+}
+
+export const CheckboxFilter: FC<CheckboxFilterProps> = ({
+  filterTitle,
+  uniqueFilter,
+  selectedFilter,
+  setSelectedFilter,
+}) => {
+  const [isUpMobileBrands, setIsUpMobileBrands] = useState<string[]>([]);
+  const [open, setOpen] = useState(false);
+
+  const { isUpMobile, isUpTablet, isUpLaptop } = useBreakpoints();
+
+  const handleBrandChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const index = selectedFilter.indexOf(event.target.value);
+    if (index === -1) {
+      setSelectedFilter([...selectedFilter, event.target.value]);
+    } else {
+      setSelectedFilter(
+        selectedFilter.filter((brand) => brand !== event.target.value)
+      );
+    }
+  };
+
+  const onMobileClickHandle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const index = isUpMobileBrands.indexOf(event.target.value);
+    if (index === -1) {
+      setIsUpMobileBrands([...isUpMobileBrands, event.target.value]);
+    } else {
+      setIsUpMobileBrands(
+        isUpMobileBrands.filter((brand) => brand !== event.target.value)
+      );
+    }
+  };
+
+  const formControlMobile = (
+    <FormControl sx={styles.fomControl}>
+      <FormGroup>
+        {uniqueFilter.map((item, index) => (
+          <FormControlLabel
+            key={index}
+            label={item}
+            value={item.replace(/\s/g, "-")}
+            sx={styles.formControlLabel}
+            control={
+              <Checkbox
+                checked={isUpMobileBrands.includes(`${item}`)}
+                onChange={onMobileClickHandle}
+                value={item.replace(/\s/g, "-")}
+                sx={styles.checkbox}
+              />
+            }
+          />
+        ))}
+      </FormGroup>
+    </FormControl>
+  );
+
+  const formControl = (
+    <FormControl>
+      <FormGroup>
+        {uniqueFilter.map((item, index) => (
+          <FormControlLabel
+            key={index}
+            label={item}
+            value={item}
+            sx={styles.formControlLabel}
+            control={
+              <Checkbox
+                checked={selectedFilter.includes(`${item}`)}
+                onChange={handleBrandChange}
+                value={`${item}`}
+                sx={styles.checkbox}
+              />
+            }
+          />
+        ))}
+      </FormGroup>
+    </FormControl>
+  );
+
+  return (
+    <Box sx={styles.root}>
+      <Box sx={styles.titleBox} onClick={() => setOpen(!open)}>
+        <Typography variant="h6" sx={styles.title}>
+          {filterTitle}
+        </Typography>
+        {isUpTablet && !isUpLaptop && (
+          <IconButton onClick={() => setOpen(!open)} sx={styles.arrowIcon}>
+            {open ? <ArrowUpIcon /> : <ArrowDownIcon />}
+          </IconButton>
+        )}
+        {isUpMobile && !isUpTablet && !isUpLaptop && (
+          <Button
+            onClick={() => setSelectedFilter(isUpMobileBrands)}
+            sx={styles.applyButton}
+          >
+            <Typography color="custom.primary">Apply</Typography>
+          </Button>
+        )}
+      </Box>
+      {isUpMobile && !isUpTablet && !isUpLaptop && formControlMobile}
+      {open && isUpTablet && !isUpLaptop && formControl}
+      {isUpLaptop && formControl}
+    </Box>
+  );
 };
--- src/components/unsorted/CheckboxFilter/styles.ts
+++ src/components/unsorted/CheckboxFilter/styles.ts
@@ -1,3 +1,45 @@
 import { SxStyles } from "../../../types/styles";
+import { basicTheme } from "../../../theme";
 
-export const styles: SxStyles = {};
+export const styles: SxStyles = {
+  root: {},
+  titleBox: {
+    display: "flex",
+    mb: 5,
+    width: "50%",
+  },
+  applyButton: {
+    ml: "auto",
+    mr: "9px",
+    minWidth: "auto",
+  },
+  arrowIcon: {
+    ml: "auto",
+  },
+  fomControl: {
+    width: "50%",
+  },
+  formControlLabel: {
+    mr: 0,
+    ".Mui-checked": {
+      color: "custom.primary",
+    },
+    display: "flex",
+    ml: 0,
+    ".MuiCheckbox-root": {
+      order: 1,
+      ml: "auto",
+    },
+    [basicTheme.breakpoints.up("tablet")]: {
+      ml: "-11px",
+      mr: "auto",
+      ".MuiCheckbox-root": {
+        order: "-1",
+        mr: "0",
+      },
+      ".MuiFormControlLabel-label": {
+        order: 1,
+      },
+    },
+  },
+};
